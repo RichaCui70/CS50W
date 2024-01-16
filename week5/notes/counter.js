e8b52e0af8b1b75cbce778e084569d029a9eb305
Richard Cui <cuir@uoguelph.ca>
2024-01-16 10:33:15 -0500

    updating week5 notes

--- week5/notes/counter.js
+++ week5/notes/counter.js
@@ -1,10 +1,14 @@
-let counter = 0;
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
 function count() {
-    document.querySelector('h1').innerHTML = ++counter
-    if (counter % 10 === 0) {
-        alert(`counter is now ${counter}!`)
-    }
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 }
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').onclick = count;
+
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter')
 })
